b0f32d37384c1a81faeea03a8588d2d427b6b7ea
Ayesha Jahangir <ayeshajahangirrr@gmail.com>
2025-07-25 21:09:18 +0500

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,8 @@
 from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
+from django.contrib import admin
+
 
 urlpatterns = [
     # your API routes
